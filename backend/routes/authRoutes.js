3beac99d0dd84561d41d438f7a6e5edc0694211e
Ruchi <118293312+Ruchi3105@users.noreply.github.com>
2025-03-24 13:39:57 -0800

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -44,8 +44,8 @@ router.post("/login", async (req, res) => {
 
     res.cookie("auth_token", token, {
       httpOnly: true,
-      secure: false,
-      sameSite: "Strict",
+      secure: true,
+      sameSite: "None",
     });
 
     const { password, ...others } = user._doc;
@@ -58,8 +58,8 @@ router.post("/login", async (req, res) => {
 router.post("/logout", (req, res) => {
   res.clearCookie("auth_token", {
     httpOnly: true,
-    secure: false,
-    sameSite: "Strict",
+    secure: true,
+    sameSite: "None",
   });
   res.status(200).json({ message: "Logged out successfully" });
 });
