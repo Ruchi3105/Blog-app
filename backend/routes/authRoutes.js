5166b8f16d223a2bfce2c21cbc93fa50f09033b4
Ruchi <118293312+Ruchi3105@users.noreply.github.com>
2025-03-25 11:39:30 -0800

    cookie secure set to true

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -44,7 +44,7 @@ router.post("/login", async (req, res) => {
 
     res.cookie("auth_token", token, {
       httpOnly: true,
-      secure: false,
+      secure: true,
       sameSite: "Strict",
     });
 
@@ -58,7 +58,7 @@ router.post("/login", async (req, res) => {
 router.post("/logout", (req, res) => {
   res.clearCookie("auth_token", {
     httpOnly: true,
-    secure: false,
+    secure: true,
     sameSite: "Strict",
   });
   res.status(200).json({ message: "Logged out successfully" });
