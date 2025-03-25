00708288465df90c77d1a5bdbe488419f24f8a6e
Ruchi <118293312+Ruchi3105@users.noreply.github.com>
2025-03-25 11:50:34 -0800

    same site none set

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -45,7 +45,7 @@ router.post("/login", async (req, res) => {
     res.cookie("auth_token", token, {
       httpOnly: true,
       secure: true,
-      sameSite: "Strict",
+      sameSite: "None",
     });
 
     const { password, ...others } = user._doc;
@@ -59,7 +59,7 @@ router.post("/logout", (req, res) => {
   res.clearCookie("auth_token", {
     httpOnly: true,
     secure: true,
-    sameSite: "Strict",
+    sameSite: "None",
   });
   res.status(200).json({ message: "Logged out successfully" });
 });
