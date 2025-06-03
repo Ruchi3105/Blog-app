0edaad6407c32f294b3b9effc1169311bf50ac72
Ruchi3105 <ruchi.cse31@gmail.com>
2025-06-03 14:59:37 +0530

    env updated

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(cors({ origin: "https://blog-app-frontend-jhla.onrender.com", credentials: true }));
+app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
 
 connectDB();
 
